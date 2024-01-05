2a84d0fa16b2e7337e58965e2e81a9629e5f2aa1
Youngbae1126 <rcplaza2663@gmail.com>
2024-01-05 11:55:09 +0900

    Docs: 페이지 라우트 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Main from './pages/LoginHome';
+import Feed from './pages/Feed';
+import Search from './pages/Search';
+import Mypage from './pages/Mypage';
+
+// COMPONENTS
+import Header from './components/Header';
 
 function App() {
 	return (
-		<div className="App">
-			<h1>여기는 페이지입니다!! 수정</h1>
-			<h2>Hello World!!</h2>
-			<h3>😀😀hi hello</h3>
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/main" element={<Main />} />
+					<Route path="/feed" element={<Feed />} />
+					<Route path="/search" element={<Search />} />
+					<Route path="/mypage" element={<Mypage />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
 
