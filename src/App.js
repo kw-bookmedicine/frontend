33bb1f63f3f0cb2d815fc2e432b89f3fcee0ae90
Youngbae1126 <rcplaza2663@gmail.com>
2024-01-05 15:54:23 +0900

    :memo: Docs: 페이지 라우팅 주소 변경

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Main from './pages/LoginHome';
+import Main from './pages/Main';
 import Feed from './pages/Feed';
 import Search from './pages/Search';
 import Mypage from './pages/Mypage';
@@ -21,7 +21,8 @@ function App() {
 	return (
 		<BrowserRouter>
 			<div className="App">
-				<Header />
+				<GlobalStyles />
+
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/main" element={<Main />} />
@@ -30,7 +31,7 @@ function App() {
 					<Route path="/mypage" element={<Mypage />} />
 
 					<Route path="/login" element={<Login />} />
-					<Route path="/join2" element={<Join2 />} />
+					<Route path="/join" element={<Join2 />} />
 					<Route path="/test" element={<Registration />} />
 				</Routes>
 			</div>
