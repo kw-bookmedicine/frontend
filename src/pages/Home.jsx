2da9d1e8810531ba5d9c29324277338bf45ce38a
Youngbae1126 <rcplaza2663@gmail.com>
2024-01-15 00:06:49 +0900

    :art: Design: 로그인 전 홈 화면 감정에 따른 책 리스트 및 footer 디자인 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // COMPONENTS
 import Btn from '../components/Button';
+import EmotionItem from '../components/EmotionItem';
+import EmotionBookList from '../components/EmotionBookList';
 
 // STYLES
 import '../styles/HomeStyles.css';
@@ -26,13 +28,8 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/bundle';
 
-// Assets
-import happy from '../assets/icon-color-happy.png';
-import smile from '../assets/icon-color-smile.png';
-import angry from '../assets/icon-color-angry.png';
-import lonely from '../assets/icon-color-lonely.png';
-import stress from '../assets/icon-color-stress.png';
-import sad from '../assets/icon-color-sad.png';
+const env = process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || '';
 
 const featSlider = [
 	{
@@ -55,6 +52,39 @@ const featSlider = [
 	},
 ];
 
+const emotionList = [
+	{
+		emotion_id: 1,
+		emotion_img: '/icon/angry.png',
+		emotion_text: '화날 ',
+	},
+	{
+		emotion_id: 2,
+		emotion_img: '/icon/sad.png',
+		emotion_text: '슬플 ',
+	},
+	{
+		emotion_id: 3,
+		emotion_img: '/icon/smile.png',
+		emotion_text: '기쁠 ',
+	},
+	{
+		emotion_id: 4,
+		emotion_img: '/icon/happy.png',
+		emotion_text: '즐거울 ',
+	},
+	{
+		emotion_id: 5,
+		emotion_img: '/icon/stress.png',
+		emotion_text: '불안할 ',
+	},
+	{
+		emotion_id: 6,
+		emotion_img: '/icon/lonely.png',
+		emotion_text: '외로울 ',
+	},
+];
+
 const StyledSwiper = styled(Swiper)`
 	.swiper-button-next {
 		background: url(/public/icon/home-right-arrow.png) no-repeat;
@@ -70,13 +100,28 @@ const StyledSwiper = styled(Swiper)`
 `;
 
 const Home = () => {
+	const [emotion, setEmotion] = useState(3);
+	const [text, setText] = useState('기쁠 때 추천하는 책이예요!');
+	// state를 두어서 불러오는 api url 변경하면 될 듯
+
+	const handleClickEmotion = (emotion) => {
+		setEmotion(emotion);
+		setText(
+			emotionList.map((data) => {
+				if (emotion === data.emotion_id)
+					return `${data.emotion_text} 때 추천하는 책이예요!`;
+				else return ' ';
+			}),
+		);
+	};
+
 	return (
 		<>
 			<section className="home">
 				<section className="home_header_section">
 					<div className="home_header">
 						<div className="header_logo">
-							책국
+							<Link to={'/'}>책국</Link>
 							<div className="header_btn"></div>
 						</div>
 						<Btn text={'로그인'} type="login" id="home-header-btn" />
@@ -85,16 +130,14 @@ const Home = () => {
 				<section className="intro">
 					<section className="home_main">
 						<div className="home_main_wrapper">
-							<div className="home_main_img">
-								<div className="home_main_title_wrapper">
-									<div className="big_title">
-										당신의 마음에
-										<br />한 발짝 가까이
-									</div>
-									<div className="small_title">
-										<br />
-										당신의 오늘은 어떤가요?
-									</div>
+							<div className="home_main_title_wrapper">
+								<div className="big_title">
+									당신의 마음에
+									<br />한 발짝 가까이
+								</div>
+								<div className="small_title">
+									<br />
+									당신의 오늘은 어떤가요?
 								</div>
 							</div>
 						</div>
@@ -148,20 +191,40 @@ const Home = () => {
 
 					<section className="home_emotion">
 						<div className="emotion_wrapper">
-							<div className="emotion_up">
+							<div className="emotion_up_wrapper">
 								<div className="emotion_title">
 									감정에 따라
 									<br />
 									책을 읽어봐요!
 								</div>
-								<div className="emotion_slide"></div>
-								<div className="emotion_text">기쁠 때 추천하는 책이예요!</div>
+								<div className="emotion_slide_wrapper">
+									<div className="emotion_slide">
+										{emotionList.map((it) => (
+											<EmotionItem
+												key={it.emotion_id}
+												{...it}
+												onClick={handleClickEmotion}
+												isSelected={it.emotion_id === emotion}
+											/>
+										))}
+									</div>
+								</div>
+								<div className="emotion_text">{text}</div>
 							</div>
-							<div className="emotion_down">
-								<div className="emotionBook_slide"></div>
+							<div className="emotion_bottom_wrapper">
+								<div className="emotionBook_slide">
+									<EmotionBookList text={text} />
+								</div>
 							</div>
 						</div>
 					</section>
+				</section>
+				<section className="home_footer">
+					<div className="home_footer_wrapper">
+						<div className="home_footer_inner">
+							Copyright © 2024 책국 All Rights Reserved.
+						</div>
+					</div>
 				</section>
 			</section>
 		</>
