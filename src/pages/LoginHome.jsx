6d987453685f369fff5b5c137d4a834360fa3c76
Daewony <alexnori.com@gmail.com>
2024-01-23 17:04:01 +0900

    :sparkles: Feat: 처방전의 오늘 날짜 적용 및 처방전 css 수정

--- src/pages/LoginHome.jsx
+++ src/pages/LoginHome.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import PrescriptionReviewCard from "../components/PrescriptionReviewCard";
 import Slider from "../components/Slider";
@@ -5,6 +6,21 @@ import "../styles/LoginHome.css";
 import { Link } from "react-router-dom";
 
 const LoginHome = () => {
+  // 현재 날짜를 가져오기 위해 Date 객체를 사용
+  const currentDate = new Date();
+
+  // 날짜를 포맷에 맞게 문자열로 만들기
+  const formattedDate = `${currentDate.getFullYear()}.${(
+    currentDate.getMonth() + 1
+  )
+    .toString()
+    .padStart(2, "0")}.${currentDate.getDate().toString().padStart(2, "0")}`;
+
+  // 모달창에서 오늘의 감정 선택 후, 감정에 따른 todayEmotion 수정
+  const [todayEmotion, setTodayEmotion] = useState("즐거운");
+  
+  
+
   return (
     <>
       <Header />
@@ -20,6 +36,7 @@ const LoginHome = () => {
           </div> */}
         </div>
 
+        {/*  */}
         <div className="LoginHome-main-container">
           <div className="LoginHome-today">
             <div className="LoginHome-today-item">
@@ -28,7 +45,7 @@ const LoginHome = () => {
                 <div className="LoginHome-today-item-contents-text">
                   오늘
                   <p className="LoginHome-today-item-contents-text-hightlight">
-                    즐거운
+                    {todayEmotion}
                   </p>
                   당신을 위한 책 처방
                 </div>
@@ -36,7 +53,7 @@ const LoginHome = () => {
                 <h3 className="LoginHome-today-item-contents-title">책 제목</h3>
                 <h4>저자</h4>
                 <h3 className="LoginHome-today-item-contents-date">
-                  2024.01.13.
+                  {formattedDate}
                 </h3>
               </div>
             </div>
@@ -95,7 +112,9 @@ const LoginHome = () => {
                   bookImg={""}
                   bookCategory={"서시"}
                   author={"윤동주"}
-                  review={"죽는 날까지 하늘을 우러러 한 점 부끄럼이 없기를"}
+                  review={
+                    "죽는 날까지 하늘을 우러러 한 점 부끄럼이 없기를죽는 날까지 하늘을 우러러 한 점 부끄럼이 없기를죽는 날까지 하늘을 우러러 한 점 부끄럼이 없기를죽는 날까지 하늘을 우러러 한 점 부끄럼이 없기를죽는 날까지 하늘을 우러러 한 점 부끄럼이 없기를"
+                  }
                   userImg={""}
                   userNickname={"유저 닉네임"}
                 />
