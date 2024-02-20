73e37d669aeec21d8d287a7e4f2d146a2eebf576
Youngbae1126 <rcplaza2663@gmail.com>
2024-02-20 17:24:56 +0900

    :sparkles: Feat: 로그인 되지 않은 상태에서 접근 제한 구현

--- src/components/Button.js
+++ src/components/Button.js
@@ -23,9 +23,19 @@ const Button = ({ text, type }) => {
 	if (type === 'nickname' || type === 'password' || type === 'job') {
 		btnUrl = `/edit/${type}`;
 		btnType = 'edit2';
+	} else if (type === 'logout' || type === 'logout2') {
+		// localStorage.clear();
+		btnUrl = '/';
 	} else {
 		btnUrl = `/${btnType}`;
 	}
+
+	const logout = () => {
+		let token = localStorage.getItem('token');
+		console.log('logout!');
+		localStorage.clear();
+		window.location.replace('http://localhost:3000/');
+	};
 
 	const renderButton = (url, type) => {
 		if (type === 'add' || type === 'delete') {
@@ -44,11 +54,24 @@ const Button = ({ text, type }) => {
 				// </div>
 			);
 		} else {
-			return (
-				<button className={styles[`Btn-${type}`]}>
-					<Link to={url}>{text}</Link>
-				</button>
-			);
+			if (type === 'logout') {
+				return (
+					<button
+						onClick={() => {
+							logout();
+						}}
+						className={styles[`Btn-${type}`]}
+					>
+						{text}
+					</button>
+				);
+			} else {
+				return (
+					<button className={styles[`Btn-${type}`]}>
+						<Link to={url}>{text}</Link>
+					</button>
+				);
+			}
 		}
 	};
 
