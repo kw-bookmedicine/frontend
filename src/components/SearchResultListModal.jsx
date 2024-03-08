df5827e3f324a04a23495f9ce0aa734734a81635
Daewony <alexnori.com@gmail.com>
2024-03-08 17:48:49 +0900

    Feat: 검색 리스트 클릭 시, 해당 책 상세 페이지로 이동

--- src/components/SearchResultListModal.jsx
+++ src/components/SearchResultListModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../styles/SearchResultList.css";
+import { Link } from 'react-router-dom';
 
 const SearchResultListModal = ({ book, type, updateBook, addInput }) => {
   let listType = ["myBook"].includes(type) ? type : "search";
@@ -19,25 +20,25 @@ const SearchResultListModal = ({ book, type, updateBook, addInput }) => {
           let author = item && item.author;
           let thumbnail = item && item.imageUrl;
           let keyword = item && item.name;
+          console.log(`/book-detial/${title}`);
 
           // 책 제목 && 작가 UI
           if (title !== undefined) {
             return (
               <ul key={item.id} className={`${listType}-result-container`}>
-                <li
-                  className={`${listType}-result-list`}
-                  onClick={() => {
-                    updateBookTitle(title);
-                  }}
-                >
-                  <img src={thumbnail} alt="" />
-                  <div className={`${listType}-result-item`}>
-                    <h1 className={`${listType}-result-item-title`}>{title}</h1>
-                    <h1 className={`${listType}-result-item-author`}>
-                      {author}
-                    </h1>
-                  </div>
-                </li>
+                <Link to={`/book-detial/${title}`}>
+                  <li className={`${listType}-result-list`}>
+                    <img src={thumbnail} alt="" />
+                    <div className={`${listType}-result-item`}>
+                      <h1 className={`${listType}-result-item-title`}>
+                        {title}
+                      </h1>
+                      <h1 className={`${listType}-result-item-author`}>
+                        {author}
+                      </h1>
+                    </div>
+                  </li>
+                </Link>
               </ul>
             );
           }
