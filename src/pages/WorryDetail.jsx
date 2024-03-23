71280b91dd1aa31288dad72372aec6c3bbffc858
Youngbae1126 <rcplaza2663@gmail.com>
2024-03-23 23:57:27 +0900

    :sparkles: Feat: 증상 설명 페이지 라우팅

---
+++ src/pages/WorryDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// COMPONENTS
+import Header from '../components/Header';
+
+const WorryDetail = () => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
+
+export default WorryDetail;
