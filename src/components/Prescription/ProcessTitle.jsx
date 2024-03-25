96abd3db1e91753a10c13bd72cd995cea278df16
Youngbae1126 <rcplaza2663@gmail.com>
2024-03-25 19:51:43 +0900

    :art: Design: Progress bar 타이틀 디자인 추가

--- src/components/Prescription/ProcessTitle.jsx
+++ src/components/Prescription/ProcessTitle.jsx
@@ -5,26 +5,37 @@ import { Link } from 'react-router-dom';
 import '../../styles/Counseling/ProcessTitle.css';
 
 const ProcessTitle = ({ type }) => {
-	let processTitleType = ['process'].includes(type) ? type : 'default';
-
 	const renderTitle = (type) => {
 		if (type === 'normal') {
 			return (
 				<>
-					<div className={`process_${type}_title`}>진단서</div>
-					<div className={`process_${type}_subTitle`}>
-						<span id={`${type}_nickname`}>이름 없는 새</span> 님의 진단서
+					<div className={`process_${type}_title_wrapper`}>
+						<div className={`process_${type}_title`}>진단서</div>
+						<div className={`process_${type}_subTitle`}>
+							<span id={`${type}_nickname`}>이름 없는 새</span> 님의 진단서
+						</div>
+					</div>
+				</>
+			);
+		} else {
+			return (
+				<>
+					<div className="process_title_wrapper">
+						<div className="process_title">
+							<span id="process_nickname">이름 없는 새</span> 님을 위한 처방전
+							작성 중...
+						</div>
+						<div className="progress_bar_wrapper">
+							<progress id="progress" value="50" min="0" max="100"></progress>
+							<p id="progress_text">50%</p>
+						</div>
 					</div>
 				</>
 			);
 		}
 	};
 
-	return (
-		<>
-			<div className="process_title_wrapper">{renderTitle(type)}</div>
-		</>
-	);
+	return <>{renderTitle(type)}</>;
 };
 
 export default ProcessTitle;
