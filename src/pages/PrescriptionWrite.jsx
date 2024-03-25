991316864becaeb273771fd7b1051b23184e904e
Youngbae1126 <rcplaza2663@gmail.com>
2024-03-25 19:29:35 +0900

    :art: Design: 증상 설명 페이지 1차 디자인 구현

---
+++ src/pages/PrescriptionWrite.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// COMPONENTS
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+// STYLE
+import '../styles/Counseling/PrescriptionWrite.css';
+
+const PrescriptionWrite = () => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
+
+export default PrescriptionWrite;
