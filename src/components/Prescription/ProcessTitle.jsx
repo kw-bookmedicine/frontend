ee8df77260b7cd1e33b01e9b1a10a333648593d7
Youngbae1126 <rcplaza2663@gmail.com>
2024-03-25 19:28:15 +0900

    :art: Design: 타입별 제목 컴포넌트 다르게 렌더링

--- src/components/Prescription/ProcessTitle.jsx
+++ src/components/Prescription/ProcessTitle.jsx
@@ -7,7 +7,24 @@ import '../../styles/Counseling/ProcessTitle.css';
 const ProcessTitle = ({ type }) => {
 	let processTitleType = ['process'].includes(type) ? type : 'default';
 
-	return <div className="process_title_wrapper"></div>;
+	const renderTitle = (type) => {
+		if (type === 'normal') {
+			return (
+				<>
+					<div className={`process_${type}_title`}>진단서</div>
+					<div className={`process_${type}_subTitle`}>
+						<span id={`${type}_nickname`}>이름 없는 새</span> 님의 진단서
+					</div>
+				</>
+			);
+		}
+	};
+
+	return (
+		<>
+			<div className="process_title_wrapper">{renderTitle(type)}</div>
+		</>
+	);
 };
 
 export default ProcessTitle;
