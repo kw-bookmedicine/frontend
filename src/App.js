d67e4fee95cc91b35eb5756e5a086fd567b913c1
Daewony <alexnori.com@gmail.com>
2024-03-26 17:06:28 +0900

    :sparkles: Feat: 검색 결과에 나온 모든 책들의 키워드로 필터링 검색 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,93 +1,93 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // PAGES
-import Home from './pages/Home';
-import Main from './pages/LoginHome';
-import Feed from './pages/Feed';
-import MyFeed from './pages/MyFeed';
-import Search from './pages/Search';
-import Mypage from './pages/Mypage';
-import BookDetail from './pages/BookDetail';
-import Login from './pages/Login';
-import Join from './pages/Join';
-import Signup2 from './pages/Signup2';
-import Signup3 from './pages/Signup3';
-import FindUserInfo from './pages/FindUserInfo';
-import Registration from './pages/Registration';
-import BookList from './pages/BookList';
-import SmallCategory from './pages/SmallCategory';
-import Edit from './pages/Edit';
-import UserInfo from './pages/UserInfo';
+import Home from "./pages/Home";
+import Main from "./pages/LoginHome";
+import Feed from "./pages/Feed";
+import MyFeed from "./pages/MyFeed";
+import Search from "./pages/Search";
+import Mypage from "./pages/Mypage";
+import BookDetail from "./pages/BookDetail";
+import Login from "./pages/Login";
+import Join from "./pages/Join";
+import Signup2 from "./pages/Signup2";
+import Signup3 from "./pages/Signup3";
+import FindUserInfo from "./pages/FindUserInfo";
+import Registration from "./pages/Registration";
+import BookList from "./pages/BookList";
+import SmallCategory from "./pages/SmallCategory";
+import Edit from "./pages/Edit";
+import UserInfo from "./pages/UserInfo";
 
 // 처방전 관련 페이지들
-import Counseling from './pages/Counseling';
+import Counseling from "./pages/Counseling";
 
 // STYLE
-import GlobalStyles from './styles/GlobalStyles';
-import './App.css';
-import ScrollTop from './components/ScrollTop';
-import LoginLayout from './components/LoginLayout';
-import LoginFindResult from './pages/IdFindResult';
-import PasswordFind from './pages/PasswordFind';
-import PasswordFindResult from './pages/PasswordFindResult';
-import IdFind from './pages/IdFind';
-import LoginContextProvider from './contexts/LoginContextProvider';
-import SearchResult from './pages/SearchResult';
+import GlobalStyles from "./styles/GlobalStyles";
+import "./App.css";
+import ScrollTop from "./components/ScrollTop";
+import LoginLayout from "./components/LoginLayout";
+import LoginFindResult from "./pages/IdFindResult";
+import PasswordFind from "./pages/PasswordFind";
+import PasswordFindResult from "./pages/PasswordFindResult";
+import IdFind from "./pages/IdFind";
+import LoginContextProvider from "./contexts/LoginContextProvider";
+import SearchResult from "./pages/SearchResult";
 
 function App() {
-	// 브라우저 새로고침 스크롤 이벤트
-	useEffect(() => {
-		window.onbeforeunload = function pushRefresh() {
-			window.scrollTo(0, 0);
-		};
-	}, []);
+  // 브라우저 새로고침 스크롤 이벤트
+  useEffect(() => {
+    window.onbeforeunload = function pushRefresh() {
+      window.scrollTo(0, 0);
+    };
+  }, []);
 
-	https: return (
-		<BrowserRouter>
-			<LoginContextProvider>
-				<div className="App">
-					<GlobalStyles />
-					<ScrollTop />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/logout" element={<Home />} />
-						<Route path="/main" element={<Main />} />
-						<Route path="/feed" element={<Feed />} />
-						<Route path="/search" element={<Search />} />
-						<Route path="/search/result/:title" element={<SearchResult />} />
-						<Route path="/mypage" element={<Mypage />} />
-						<Route path="/myfeed" element={<MyFeed />} />
-						<Route path="/edit" element={<Edit />} />
-						<Route path="/edit/:page" element={<UserInfo />} />
+  https: return (
+    <BrowserRouter>
+      <LoginContextProvider>
+        <div className="App">
+          <GlobalStyles />
+          <ScrollTop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/logout" element={<Home />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/search/result/:title" element={<SearchResult />} />
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/myfeed" element={<MyFeed />} />
+            <Route path="/edit" element={<Edit />} />
+            <Route path="/edit/:page" element={<UserInfo />} />
 
-						<Route path="/counseling" element={<Counseling />} />
+            <Route path="/counseling" element={<Counseling />} />
 
-						<Route element={<LoginLayout />}>
-							<Route path="/id-find" element={<IdFind />} />
-							<Route path="/id-find-result" element={<LoginFindResult />} />
-							<Route path="/password-find" element={<PasswordFind />} />
-							<Route
-								path="/password-find-result"
-								element={<PasswordFindResult />}
-							/>
-						</Route>
-						<Route path="/test" element={<Registration />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/signup" element={<Join />} />
-						<Route path="/signup/1" element={<Signup2 />} />
-						<Route path="/signup/2" element={<Signup3 />} />
-						{/* <Route path="/find/user/info/:page" element={<FindUserInfo />} /> */}
-						<Route path="/test" element={<Registration />} />
+            <Route element={<LoginLayout />}>
+              <Route path="/id-find" element={<IdFind />} />
+              <Route path="/id-find-result" element={<LoginFindResult />} />
+              <Route path="/password-find" element={<PasswordFind />} />
+              <Route
+                path="/password-find-result"
+                element={<PasswordFindResult />}
+              />
+            </Route>
+            <Route path="/test" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Join />} />
+            <Route path="/signup/1" element={<Signup2 />} />
+            <Route path="/signup/2" element={<Signup3 />} />
+            {/* <Route path="/find/user/info/:page" element={<FindUserInfo />} /> */}
+            <Route path="/test" element={<Registration />} />
 
-						<Route path="/book-detail" element={<BookDetail />} />
-						<Route path="/book/list/:title" element={<BookList />} />
-						<Route path="/book/:title/:category" element={<SmallCategory />} />
-					</Routes>
-				</div>
-			</LoginContextProvider>
-		</BrowserRouter>
-	);
+            <Route path="/book-detail" element={<BookDetail />} />
+            <Route path="/book/list/:title" element={<BookList />} />
+            <Route path="/book/:title/:category" element={<SmallCategory />} />
+          </Routes>
+        </div>
+      </LoginContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -7,6 +7,7 @@ import starIcon from "../assets/icons8-별-30 (1).png";
 import api from "./../services/api";
 import Pagination from "../components/Pagination";
 import SearchBox from "../components/SearchBox";
+import Pill from "../components/Pill";
 
 const SearchResult = () => {
   const navigate = useNavigate();
@@ -15,6 +16,7 @@ const SearchResult = () => {
   const [books, setBooks] = useState([]); // 책 정보
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
   const [booksPerPage, setBooksPerPage] = useState(10); // 페이지당 책 수
+
   const [loading, setLoading] = useState(false); // 로딩 상태
 
   const [input, setInput] = useState(""); // 검색 데이터
@@ -26,7 +28,10 @@ const SearchResult = () => {
   const [selectedKeywords, setSelectedKeywords] = useState([]);
   const [selectedKeywordSet, setSelectedKeywordSet] = useState(new Set());
 
-  const [totalBooksOfKeywords, setTotalBooksOfKeywords] = useState([]);
+
+  const [searchedSelectedKeywords, setSearchedSelectedKeywords] = useState([]);
+  
+
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -63,23 +68,45 @@ const SearchResult = () => {
   const searchBook = (evt) => {
     if (evt.key === "Enter") {
       fetchBooks(input);
-      if (input.length > 0 && selectedKeywords.length === 0) navigate(`/search/result/${input}`);
-      if (selectedKeywords.length > 0) navigate(`/search/result/${selectedKeywords.join(' ')} ${input}`);
-      if(input.length === 0 && searchType !== "keyword") alert("검색 키워드가 없습니다!")
-      if (searchType === "keyword" && selectedKeywords.length === 0) alert("키워드를 선택하여 검색해주세요!")
-        setIsShow(false);
+      if (input.length > 0 && selectedKeywords.length === 0)
+        navigate(`/search/result/${input}`);
+      if (selectedKeywords.length > 0)
+        navigate(`/search/result/${selectedKeywords.join(" ")} ${input}`);
+      if (input.length === 0 && searchType !== "keyword")
+        alert("검색 키워드가 없습니다!");
+      if (searchType === "keyword" && selectedKeywords.length === 0)
+        alert("키워드를 선택하여 검색해주세요!");
+      setIsShow(false);
     }
   };
 
   // 키워드로 어떻게 담지?
 
- 
+  // const { searchedSelectedKeywords,removeKeyword } = useSearchedSelectedKeywords();
+  // console.log(searchedSelectedKeywords)
 
   // 현재 책들 정보
+  // const filteredBooks = books.filter(
+  //   (book) =>
+  //     book.bookKeywordList.some((keyword) =>
+  //       searchedSelectedKeywords.includes(keyword.name)
+  //     ) || searchedSelectedKeywords.includes(book.middleCategoryName)
+  // );
+  const filteredBooks = books.filter((book) =>
+    searchedSelectedKeywords.every(
+      (keyword) =>
+        book.bookKeywordList.some(
+          (bookKeyword) => bookKeyword.name === keyword
+        ) || keyword === book.middleCategoryName
+    )
+  );
+
+  console.log("Filtered Books: ", filteredBooks);
+
   const indexOfLastBook = currentPage * booksPerPage;
   const indexOfFirstBook = indexOfLastBook - booksPerPage;
-  // console.log(indexOfFirstBook, indexOfLastBook);
-  const currentBooks = books.slice(indexOfFirstBook, indexOfLastBook);
+  // const currentBooks = books.slice(indexOfFirstBook, indexOfLastBook);
+  const currentBooks = filteredBooks.slice(indexOfFirstBook, indexOfLastBook);
 
   // 페이지 변경
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
@@ -92,7 +119,6 @@ const SearchResult = () => {
           // `/api/search/book?title=${title}&target=page&page=${currentPage-1}&size=${booksPerPage}`
           `/api/search/book?title=${title}&target=page&page=0&size=999`
         );
-        // console.log(response.data);
         setBooks(response.data);
         setLoading(false);
       } catch (error) {
@@ -101,10 +127,9 @@ const SearchResult = () => {
     };
     getSearchResults();
   }, [title]);
-  
+
   let searchResultsCount = books.length;
   searchResultsCount = searchResultsCount.toLocaleString();
-  let searchResultsKeywordCount = 123;
   let reviewCount = 123;
 
   // 10개, 50개, 100개에 따른 한페이지에 보여주는 책의 수
@@ -131,6 +156,13 @@ const SearchResult = () => {
     const updatedKeywordSet = new Set(selectedKeywordSet);
     updatedKeywordSet.delete(keyword);
     setSelectedKeywordSet(updatedKeywordSet);
+  };
+
+  // 검색된 키워드 삭제
+  const handleRemoveSearchedKeyword = (keyword) => {
+    setSearchedSelectedKeywords(
+      searchedSelectedKeywords.filter((k) => k !== keyword)
+    );
   };
 
   // 아래 키 기능
@@ -160,101 +192,44 @@ const SearchResult = () => {
 
         <SearchTitle id="search-title">
           <Title>
-            <span style={{ color: "#67B6C1" }}>"{title}"</span> 에 대한
-            <span style={{ color: "#67B6C1" }}>
-              {" "}
-              {searchResultsCount} 개의 검색 결과
-            </span>
+            <Highlight>"{title}"</Highlight> 에 대한
+            <Highlight> {searchResultsCount} 개의 검색 결과</Highlight>
           </Title>
         </SearchTitle>
 
         <ContentsWrap>
           <aside>
             <ContentTitle>키워드 검색</ContentTitle>
-            {/* <select name="" id="">
-              {books.map((book) => {
-                book.bookKeywordList.map((keyword, index) => {
-                  return (
-                    <option key={index} value={keyword.name}>
-                      {keyword.name}
-                    </option>
-                  );
-                });
-              })}
-            </select> */}
-            <select name="select-keyword" id="">
-              {books.flatMap((book) =>
-                book.bookKeywordList.map((keyword) => (
-                  <option value={keyword.name}>{keyword.name}</option>
-                ))
-              )}
-            </select>
-
-            <Input
-              type="text"
-              placeholder="키워드 추가"
-              style={{
-                borderRadius: "10px",
-                border: "1px solid #6B6565",
-                padding: "8px 8px 8px 12px",
-                fontSize: "1rem",
-              }}
+            <KeywordSearchBox
+              bookData={filteredBooks}
+              selectedKeywords={searchedSelectedKeywords}
+              setSelectedKeywords={setSearchedSelectedKeywords}
             />
           </aside>
 
           <section style={{ paddingLeft: "2.5rem", width: "100%" }}>
             {/* 헤더 영역 */}
-            <div
-              style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                paddingBottom: "20px",
-                borderBottom: "1px solid #c4bebe",
-              }}
-            >
+            <HeaderArea>
               <ContentTitle>
-                전체{" "}
-                <span style={{ color: "#67B6C1" }}>{searchResultsCount}건</span>
+                전체 <Highlight>{filteredBooks.length}</Highlight>건
               </ContentTitle>
               <div>
                 <div style={{ display: "flex" }}>
-                  <select
-                    name=""
-                    id=""
-                    style={{
-                      width: "130px",
-                      border: "1px solid #C0C0C0 ",
-                      padding: "0px 10px",
-                      borderRadius: "5px",
-                      // marginRight: "10px",
-                    }}
-                  >
-                    {/* <option value="" selected> */}
+                  <SelectStyled name="sortOption" id="sort-option-select">
                     <option value="popular">인기순</option>
                     <option value="ranked">평점순</option>
-                  </select>
-                  <select
-                    name=""
-                    id=""
-                    style={{
-                      width: "130px",
-                      border: "1px solid #C0C0C0 ",
-                      padding: "0px 10px",
-                      borderRadius: "5px",
-                      marginLeft: "10px",
-                      marginRight: "10px",
-                    }}
+                  </SelectStyled>
+                  <SelectStyled
+                    name="itemsPerPage"
+                    id="items-per-page-select"
                     onChange={handleSizeChange}
                     value={booksPerPage}
                   >
-                    {/* <option value="10" selected> */}
                     <option value="10">10개씩 보기</option>
                     <option value="50">50개씩 보기</option>
                     <option value="100">100개씩 보기</option>
-                  </select>
-                  <div
+                  </SelectStyled>
+                  <ViewModeContainer
                     style={{
                       display: "flex",
                       width: "75px",
@@ -263,36 +238,29 @@ const SearchResult = () => {
                       // marginLeft: "10px",
                     }}
                   >
-                    <button
+                    <ViewButton
                       onClick={() => setViewMode(true)}
-                      style={{
-                        padding: "10px",
-                        backgroundColor: viewMode ? "#d0d0d0" : "white",
-                        borderRight: "1px solid #C0C0C0",
-                      }}
+                      active={viewMode}
+                      rightBorder
                     >
                       <img
                         src="https://contents.kyobobook.co.kr/resources/fo/images/common/ink/ico_view_list_active.png"
-                        alt=""
+                        alt="리스트 뷰"
                       />
-                    </button>
-                    <button
+                    </ViewButton>
+                    <ViewButton
                       onClick={() => setViewMode(false)}
-                      style={{
-                        padding: "10px",
-                        width: "100%",
-                        backgroundColor: viewMode ? "white" : "#d0d0d0",
-                      }}
+                      active={!viewMode}
                     >
                       <img
                         src="https://contents.kyobobook.co.kr/resources/fo/images/common/ink/ico_view_img_active.png"
-                        alt=""
+                        alt="카드 뷰"
                       />
-                    </button>
-                  </div>
+                    </ViewButton>
+                  </ViewModeContainer>
                 </div>
               </div>
-            </div>
+            </HeaderArea>
 
             {/* 키워드 영역 */}
             <div
@@ -302,19 +270,34 @@ const SearchResult = () => {
               }}
             >
               <ul style={{ display: "flex" }}>
-                <SearchKeyword>
-                  {/* <div>저주</div> */}
-                  저주
-                  <button
-                    style={{
-                      backgroundColor: "#c8edf2",
-                      borderRadius: "999px",
-                      lineHeight: "5px",
+                {/* <SearchKeyword>
+                    저주
+                    <button
+                      style={{
+                        backgroundColor: "#c8edf2",
+                        borderRadius: "999px",
+                        lineHeight: "5px",
+                      }}
+                    >
+                      <img src={closeIcon} alt="" />
+                    </button>
+                  </SearchKeyword>
+                  <Pill
+                    // key={index}
+                    // text={keyword}
+                    text={"저주"}
+                    onClick={() => {
+                      handleRemoveKeyword("저주");
                     }}
-                  >
-                    <img src={closeIcon} alt="" />
-                  </button>
-                </SearchKeyword>
+                  /> */}
+                {/* {selectedKeywords.map((keyword, index) => ( */}
+                {searchedSelectedKeywords.map((keyword, index) => (
+                  <Pill
+                    key={index}
+                    text={keyword}
+                    onClick={() => handleRemoveSearchedKeyword(keyword)}
+                  />
+                ))}
               </ul>
             </div>
 
@@ -521,7 +504,7 @@ const SearchResult = () => {
             </div>
             <Pagination
               booksPerPage={booksPerPage}
-              totalBooks={books.length}
+              totalBooks={filteredBooks.length}
               paginate={paginate}
               bookTitle={title}
               currentPage={currentPage}
@@ -535,10 +518,174 @@ const SearchResult = () => {
 
 export default SearchResult;
 
+const KeywordSearchBox = ({
+  bookData,
+  selectedKeywords,
+  setSelectedKeywords,
+}) => {
+  const [totalBooksOfKeywords, setTotalBooksOfKeywords] = useState([]); // 전체 키워드
+  const [isShowModal, setIsShowModal] = useState(false); // 모달 상태
+   const [searchTerm, setSearchTerm] = useState("");
+  // 모달과 모달을 열기 위한 버튼에 대한 참조 생성
+  const modalRef = useRef(null);
+  const buttonRef = useRef(null);
+
+
+  useEffect(() => {
+    setTotalBooksOfKeywords([]); // 키워드 초기화
+    const newKeywords = bookData.flatMap((data) => {
+      const keywords = data.bookKeywordList.map((keyword) => keyword.name);
+      return [data.middleCategoryName, ...keywords];
+    });
+    setTotalBooksOfKeywords((prev) => [...prev, ...new Set(newKeywords)]); // 중복 제거
+  }, [bookData]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      // 모달이 열려 있고, 클릭한 요소가 모달 또는 버튼이 아닐 때만 모달을 닫음
+      if (
+        isShowModal &&
+        modalRef.current &&
+        !modalRef.current.contains(event.target) &&
+        !buttonRef.current.contains(event.target)
+      ) {
+        setIsShowModal(false);
+      }
+    };
+
+    // 문서에 클릭 이벤트 리스너 추가
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // 컴포넌트 언마운트 시 이벤트 리스너 제거
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isShowModal]);
+
+  const handleShowModal = () => {
+    setIsShowModal(true);
+  };
+
+  const handleSelectKeyword = (keyword) => {
+    if (!selectedKeywords.includes(keyword)) {
+      setSelectedKeywords((prevKeywords) => [...prevKeywords, keyword]);
+    }
+  };
+
+ 
+  const availableKeywords = totalBooksOfKeywords.filter(
+    (keyword) =>
+      // 선택된 키워드 포함 x, 입력된 문자 포함
+      !selectedKeywords.includes(keyword) &&
+      keyword.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <>
+      <Input
+        type="text"
+        placeholder="키워드 추가"
+        onClick={handleShowModal}
+        ref={buttonRef}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {isShowModal ? (
+        <div ref={modalRef}>
+          <KeywordSearchBoxModal
+            // keywordData={totalBooksOfKeywords}
+            keywordData={availableKeywords}
+            onSelectKeyword={handleSelectKeyword}
+          />
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+const KeywordSearchBoxModal = ({ keywordData, onSelectKeyword }) => {
+  return (
+    <>
+      <ModalStyled>
+        <ul>
+          {keywordData.map((keyword, index) => (
+            <li key={index} onClick={() => onSelectKeyword(keyword)}>
+              {keyword}
+            </li>
+          ))}
+        </ul>
+      </ModalStyled>
+    </>
+  );
+};
+
+const ModalStyled = styled.div`
+  width: 100%;
+  background-color: aliceblue;
+  cursor: pointer;
+  max-height: 300px;
+  overflow-y: auto;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 10px;
+  margin-top: 4px;
+  
+  li {
+    padding: 5px;
+  }
+`;
+
 const Main = styled.main`
   padding-top: 48px;
   max-width: 1440px;
   margin: 0 auto;
+`;
+
+// 검색창 관련
+
+// 타이틀 관련
+
+// 키워드 검색 관련
+
+// 책 결과 관련
+const ContentTitle = styled.h2`
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const Highlight = styled.span`
+  color: #67b6c1;
+`;
+
+const SelectStyled = styled.select`
+  width: 130px;
+  border: 1px solid #c0c0c0;
+  padding: 0px 10px;
+  border-radius: 5px;
+  margin-right: 10px;
+`;
+
+const HeaderArea = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #c4bebe;
+`;
+
+const ViewModeContainer = styled.div`
+  display: flex;
+  width: 75px;
+  border: 1.5px solid #c0c0c0;
+  border-radius: 5px;
+`;
+
+const ViewButton = styled.button`
+  padding: 10px;
+  width: 100%;
+  background-color: ${(props) => (props.active ? "#d0d0d0" : "white")};
+  border-right: ${(props) =>
+    props.rightBorder ? "1px solid #C0C0C0" : "none"};
 `;
 
 const SearchTitle = styled.section`
@@ -548,13 +695,7 @@ const SearchTitle = styled.section`
 const Title = styled.h1`
   font-size: 30px;
   font-weight: bold;
-
-  span {
-    color: #67b6c1;
-  }
 `;
-
-
 
 const SearchInputWrap = styled.div`
   width: 100%;
@@ -596,19 +737,12 @@ const ContentsWrap = styled.div`
   display: flex;
 `;
 
-const ContentTitle = styled.h2`
-  font-size: 20px;
-  font-weight: bold;
-  margin-bottom: 20px;
-`;
-
 const Input = styled.input`
   border-radius: 10px;
   border: 1px solid #6b6565;
   padding: 8px 8px 8px 12px;
   font-size: 1rem;
 `;
-
 
 const SearchKeyword = styled.li`
   display: flex;
