d153dd319a9085249db98538e182842264276893
Youngbae1126 <rcplaza2663@gmail.com>
2024-04-01 11:53:27 +0900

    :truck: Rename: 처방전 상세보기 페이지 처방전 폴더로 이동 및 추가

---
+++ src/pages/Prescription/PrescriptionDetail.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// COMPONENTS
+import Header from '../../components/Header.js';
+
+// STYLE
+import '../../styles/Prescription/PrescriptionDetail.css';
+
+const PrescriptionDetail = () => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
+
+export default PrescriptionDetail;
