49285978cd2436241440c0ce90fac0abdb24a139
Youngbae1126 <rcplaza2663@gmail.com>
2024-04-01 11:51:41 +0900

    :sparkles: Feat: 메뉴와 관련된 페이지로 이동 시 헤더 스타일 적용

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 // COMPONENTS
 import Btn from '../components/Button';
@@ -15,10 +15,28 @@ const Header = () => {
 		// window.location.replace('http://localhost:3000/');
 	};
 
+	// 현재 url 주소 불러옴
+	const location = useLocation();
+
+	// 해당하는 주소 url일 때, 적용될 스타일
 	const activeStyle = {
 		color: '#000',
 		fontWeight: 700,
 	};
+
+	// 고민상담페이지 -> 처방전 작성까지 주소
+	let prscrLocation = [
+		'/worry/detail',
+		'/prescription/write',
+		'/prescription/write/2',
+		'/prescription/detail',
+	];
+
+	// 검색페이지 -> 책 정보 상세보기 페이지까지 주소
+	let bookLocation = ['/book-detail', '/book/list', '/book'];
+
+	// 마이페이지 -> 내 정보 수정
+	let myLocation = ['/edit', '/edit/nickname', '/edit/password', '/edit/job'];
 
 	return (
 		<div className="head">
@@ -46,7 +64,13 @@ const Header = () => {
 						</li>
 						<li>
 							<NavLink
-								style={({ isActive }) => (isActive ? activeStyle : {})}
+								style={({ isActive }) =>
+									isActive
+										? activeStyle
+										: prscrLocation.includes(location.pathname)
+										? activeStyle
+										: {}
+								}
 								to={'/counseling'}
 							>
 								고민상담
@@ -54,7 +78,13 @@ const Header = () => {
 						</li>
 						<li>
 							<NavLink
-								style={({ isActive }) => (isActive ? activeStyle : {})}
+								style={({ isActive }) =>
+									isActive
+										? activeStyle
+										: bookLocation.includes(location.pathname)
+										? activeStyle
+										: {}
+								}
 								to={'/search'}
 							>
 								SEARCH
@@ -62,7 +92,13 @@ const Header = () => {
 						</li>
 						<li>
 							<NavLink
-								style={({ isActive }) => (isActive ? activeStyle : {})}
+								style={({ isActive }) =>
+									isActive
+										? activeStyle
+										: myLocation.includes(location.pathname)
+										? activeStyle
+										: {}
+								}
 								to={'/mypage'}
 							>
 								MYPAGE
