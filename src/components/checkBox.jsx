f1f866270bea2c2d794c15a99fa1f7c6d35e7e71
Youngbae1126 <rcplaza2663@gmail.com>
2024-05-03 01:18:46 +0900

    :art: Design: 회원가입 이용약관 페이지 UI 수정

---
+++ src/components/checkBox.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// STYLES
+import { styled } from 'styled-components';
+
+const checkBox = ({ children, type }) => {
+	if (type === 'radio') {
+		return (
+			<RadioBtn>
+				<input type="radio" />
+				{children}
+			</RadioBtn>
+		);
+	} else {
+		return (
+			<CheckBtn>
+				<input type="checkbox" />
+				{children}
+			</CheckBtn>
+		);
+	}
+};
+
+export default checkBox;
+
+const RadioBtn = styled.label`
+	font-family: var(----basic-font);
+	font-size: 19px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 20px;
+	display: flex;
+	width: 35%;
+	justify-content: flex-end;
+`;
+
+const CheckBtn = styled.label`
+	font-family: var(----basic-font);
+	font-size: 19px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 22px;
+`;
