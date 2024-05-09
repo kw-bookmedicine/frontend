23228b8a321f07891d8f8ffb06a002b9d0685919
Daewony <alexnori.com@gmail.com>
2024-05-09 17:25:33 +0900

    feat: api.prescription 게시물에 대한 처방전 조회 api 연결

--- src/components/Prescription/WidePrescriptionCard.jsx
+++ src/components/Prescription/WidePrescriptionCard.jsx
@@ -1,53 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import '../../styles/Prescription/WidePrescriptionCard.css';
+import "../../styles/Prescription/WidePrescriptionCard.css";
+import { toYYYYMMDD } from "../../util/toYYYYMMDD";
 
-const WidePrescriptionCard = () => {
-	return (
-		<>
-			<Link to={'/prescription/detail'}>
-				<div className="WidePrscrCard_container">
-					<div className="wide_prscr_left_wrapper">
-						<div className="wide_prscr_left_img">
-							<img src="/icon/pharmacy_icon.png" alt="" />
-						</div>
-						<div className="wide_prscr_left_title">
-							"새로운 곳에 적응하기 힘들어요"
-						</div>
-						<div className="wide_prscr_left_profile">
-							<div className="wide_prscr_profile_img"></div>
-							<div className="wide_prscr_profileInfo_wrapper">
-								<p className="profileInfo_nickname">나는 행복합니다</p>
-								<p className="profileInfo_date">2024.04.10.</p>
-							</div>
-						</div>
-					</div>
-					<div className="wide_prscr_mid_line"></div>
-					<div className="wide_prscr_right_wrapper">
-						<div className="wide_prscr_rl_wrapper">
-							<img src="/loading_thumbnail_x4.png" alt="" />
-						</div>
-						<div className="wide_prscr_rr_wrapper">
-							<div className="wide_prscr_rr_top_wrapper">
-								<p className="wide_prscr_bookInfo_title">책 제목</p>
-								<p className="wide_prscr_bookInfo_author">저자</p>
-								<p className="wide_prscr_bookInfo_date">출판사/출판연도</p>
-							</div>
-							<div className="wide_prscr_rr_bottom_wrapper">
-								<div className="wide_prscr_rr_reason_title">처방사유</div>
-								<div className="wide_prscr_rr_reason_box"></div>
-							</div>
-						</div>
-					</div>
-					<img
-						src="/icon/pharmacy_icon_2.png"
-						className="wide_prscr_pharmacy_icon_group"
-					/>
-				</div>
-			</Link>
-		</>
-	);
+const WidePrescriptionCard = ({ props }) => {
+  const {
+    title,
+    description,
+    createdDate,
+    nickname,
+    bookTitle,
+    author,
+    publishingHouse,
+    publishYear,
+    imageUrl,
+  } = props;
+  return (
+    <>
+      <Link to={"/prescription/detail"}>
+        <div className="WidePrscrCard_container">
+          <div className="wide_prscr_left_wrapper">
+            <div className="wide_prscr_left_img">
+              <img src="/icon/pharmacy_icon.png" alt="" />
+            </div>
+            <div className="wide_prscr_left_title">{title}</div>
+            <div className="wide_prscr_left_profile">
+              <div className="wide_prscr_profile_img"></div>
+              <div className="wide_prscr_profileInfo_wrapper">
+                <p className="profileInfo_nickname">{nickname}</p>
+                <p className="profileInfo_date">{toYYYYMMDD(createdDate)}</p>
+              </div>
+            </div>
+          </div>
+          <div className="wide_prscr_mid_line"></div>
+          <div className="wide_prscr_right_wrapper">
+            <div className="wide_prscr_rl_wrapper">
+              <img src={imageUrl || "/loading_thumbnail_x4.png"} alt="" />
+            </div>
+            <div className="wide_prscr_rr_wrapper">
+              <div className="wide_prscr_rr_top_wrapper">
+                <p className="wide_prscr_bookInfo_title">{bookTitle}</p>
+                <p className="wide_prscr_bookInfo_author">{author}</p>
+                <p className="wide_prscr_bookInfo_date">
+                  {publishingHouse || "null값"}/{publishYear || "출판연도"}
+                </p>
+              </div>
+              <div className="wide_prscr_rr_bottom_wrapper">
+                <div className="wide_prscr_rr_reason_title">처방사유</div>
+                <div className="wide_prscr_rr_reason_box">
+                  <div className="wide_prscr_rr_reason_box_content">
+                    <p>{description}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            src="/icon/pharmacy_icon_2.png"
+            className="wide_prscr_pharmacy_icon_group"
+          />
+        </div>
+      </Link>
+    </>
+  );
 };
 
 export default WidePrescriptionCard;
--- src/pages/Worry/WorryDetail.jsx
+++ src/pages/Worry/WorryDetail.jsx
@@ -1,98 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 // COMPONENTS
-import Header from './../../components/Header';
-import Title from '../../components/Prescription/ProcessTitle';
-import WidePrscrCard from '../../components/Prescription/WidePrescriptionCard';
+import Header from "./../../components/Header";
+import Title from "../../components/Prescription/ProcessTitle";
+import WidePrscrCard from "../../components/Prescription/WidePrescriptionCard";
 
 // SERVICE
-import api from '../../services/api';
+import api from "../../services/api";
 
 // STYLE
-import '../../styles/Counseling/WorryDetail.css';
+import "../../styles/Counseling/WorryDetail.css";
 
 const WorryDetail = () => {
-	const [boardData, setBoardData] = useState({});
+  const [boardData, setBoardData] = useState({});
+  const [prescriptionData, setPrescriptionData] = useState([]);
 
-	// 해당하는 고민 글 정보 가져오기
-	const fetchData = () => {
-		const currentUrl = window.location.href;
+  const currentUrl = window.location.href;
 
-		// URL에서 boardId 쿼리 파라미터 값을 추출
-		const urlParams = new URLSearchParams(new URL(currentUrl).search);
-		const boardId = urlParams.get('board');
-		try {
-			if (!boardId) {
-				console.error('주소에 boardId가 없습니다.');
-				return;
-			}
-			api.get(`/api/board/${boardId}`).then((res) => {
-				setBoardData(res.data);
-				console.log(res.data);
-			});
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  // URL에서 boardId 쿼리 파라미터 값을 추출
+  const urlParams = new URLSearchParams(new URL(currentUrl).search);
+  const boardId = urlParams.get("board");
 
-	useEffect(() => {
-		fetchData();
-	}, []);
+  // 해당하는 고민 글 정보 가져오기
+  const fetchData = () => {
+    try {
+      if (!boardId) {
+        console.error("주소에 boardId가 없습니다.");
+        return;
+      }
+      api.get(`/api/board/${boardId}`).then((res) => {
+        setBoardData(res.data);
+        console.log(res.data);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	const move = () => {
-		window.location.replace('/pre/write');
-	};
+  // 해당 고민 글의 처방전 조회
+  const fetchPrescription = async () => {
+    try {
+      const response = await api.get(
+        `/api/prescription?page=0&size=99&boardId=${boardId}`
+      );
+      setPrescriptionData(response.data);
+    } catch (error) {
+      console.log("해당 글 처방전 조회 실패", error);
+    }
+  };
 
-	return (
-		<>
-			<Header />
-			<div className="worry_detail_content">
-				<Title type={'normal'} value={boardData.nickname} />
-				<div className="worry_detail_title_wrapper">
-					<div className="wd_user_wrapper">
-						<div className="wd_user_left_wrapper">
-							<div className="wd_user_img">
-								<img
-									src="/icon/profile/basic_profile_img.svg"
-									alt="유저 프로필"
-									className="wd_user_img"
-								/>
-							</div>
-							<div className="wd_user_info_wrapper">
-								<div className="wd_user_name">{boardData.nickname}</div>
-								<div className="wd_user_date">
-									{boardData.createdDate
-										? boardData.createdDate.slice(0, 10)
-										: null}
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="wd_title_text_wrapper">{boardData.title}</div>
-				</div>
-				<div className="worry_detail_content_wrapper">
-					<div className="wd_content_detail_wrapper">
-						<span className="wd_content_detail_title">처방사유</span>
-						<div className="wd_content_detail_text">
-							{boardData.description}
-						</div>
-					</div>
-					<div className="worry_detail_prscr_wrapper">
-						<div className="wd_prscr_list_title">처방전 확인하기</div>
-						<div className="wd_prscr_list_wrapper">
-							<WidePrscrCard />
-							<WidePrscrCard />
-							<WidePrscrCard />
-						</div>
-					</div>
-					<Link to={`/prescription/write?prscrId=123`}>
-						<button className="prscr_btn">처방하러 가기</button>
-					</Link>
-				</div>
-			</div>
-		</>
-	);
+  useEffect(() => {
+    fetchData();
+    fetchPrescription();
+  }, []);
+
+  const move = () => {
+    window.location.replace("/pre/write");
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="worry_detail_content">
+        <Title type={"normal"} value={boardData.nickname} />
+        <div className="worry_detail_title_wrapper">
+          <div className="wd_user_wrapper">
+            <div className="wd_user_left_wrapper">
+              <div className="wd_user_img">
+                <img
+                  src="/icon/profile/basic_profile_img.svg"
+                  alt="유저 프로필"
+                  className="wd_user_img"
+                />
+              </div>
+              <div className="wd_user_info_wrapper">
+                <div className="wd_user_name">{boardData.nickname}</div>
+                <div className="wd_user_date">
+                  {boardData.createdDate
+                    ? boardData.createdDate.slice(0, 10)
+                    : null}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="wd_title_text_wrapper">{boardData.title}</div>
+        </div>
+        <div className="worry_detail_content_wrapper">
+          <div className="wd_content_detail_wrapper">
+            <span className="wd_content_detail_title">처방사유</span>
+            <div className="wd_content_detail_text">
+              {boardData.description}
+            </div>
+          </div>
+          <div className="worry_detail_prscr_wrapper">
+            <div className="wd_prscr_list_title">처방전 확인하기</div>
+            <div className="wd_prscr_list_wrapper">
+              {prescriptionData.map((prescription) => (
+                <WidePrscrCard key={prescription.id} props={prescription} />
+              ))}
+            </div>
+          </div>
+          <Link to={`/prescription/write?prscrId=123`}>
+            <button className="prscr_btn">처방하러 가기</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WorryDetail;
