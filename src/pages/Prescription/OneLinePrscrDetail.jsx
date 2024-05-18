97a9d2bde31f62ec24943b1fbe093e9f64e083f4
Youngbae1126 <rcplaza2663@gmail.com>
2024-05-19 03:19:55 +0900

    :tada: Init: 한 줄 처방전 상세보기 페이지, 스타일시트 생성

---
+++ src/pages/Prescription/OneLinePrscrDetail.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+// COMPONENTS
+import Header from '../../components/Header';
+
+// STYLE
+import '../../styles/Prescription/OneLinePrscrDetail.css';
+
+const OneLinePrscrDetail = () => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
+
+export default OneLinePrscrDetail;
