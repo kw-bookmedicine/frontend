a71aa23e87d520f340f51242e48849080120c9d8
Youngbae1126 <rcplaza2663@gmail.com>
2024-06-05 02:24:28 +0900

    :memo: Docs: AI처방과 다른 사람 처방 상황에 따른 UI 적용

--- src/pages/Worry/WorryDetail.jsx
+++ src/pages/Worry/WorryDetail.jsx
@@ -96,6 +96,7 @@ const WorryDetail = () => {
 	useEffect(() => {
 		fetchData();
 		fetchPrescription();
+		getAiPrscr();
 
 		if (nickname !== '') {
 			sessionStorage.setItem('nickname', nickname);
@@ -126,6 +127,42 @@ const WorryDetail = () => {
 			console.error('boardId Delete 실패:', error);
 		} finally {
 			setIsModalOpen(false);
+		}
+	};
+
+	// AI 처방전
+	const [aiPrscrArr, setPrscrArr] = useState([]);
+	const [aiPrscrId, setAiPrscrId] = useState('');
+	const [aiPrscrIsbn, setAiPrscrIsbn] = useState('');
+	const [aiPrscrTitle, setAiPrscrTitle] = useState('');
+	const [isRecommending, setIsRecommending] = useState(false);
+	const [isNoRecommendBook, setIsNoRecommendBook] = useState(false);
+
+	const getAiPrscr = () => {
+		try {
+			api
+				.get(`/api/recommend/book/boardbased?boardId=${boardId}`)
+				.then((res) => {
+					if (res.data.recommending === true) {
+						// 처방 진행 중
+						setIsRecommending(false);
+					} else {
+						if (res.data.id === null) {
+							// 추천된 책이 없음
+							setIsNoRecommendBook(true);
+						} else {
+							setPrscrArr(res.data);
+						}
+					}
+					if (res.data === '') {
+						setIsRecommending(false);
+					} else {
+						setIsRecommending(true);
+					}
+					console.log(res.data);
+				});
+		} catch (err) {
+			console.log(err);
 		}
 	};
 
@@ -187,20 +224,73 @@ const WorryDetail = () => {
 							{boardData.description}
 						</div>
 					</div>
-					{prescriptionData.length > 0 ? (
-						<div className="worry_detail_prscr_wrapper">
-							<div className="wd_prscr_list_title">처방전 확인하기</div>
-							<div className="wd_prscr_list_wrapper">
-								{prescriptionData.map((prescription) => (
-									<WidePrscrCard key={prescription.id} props={prescription} />
-								))}
+					{isRecommending ? (
+						<>
+							<div className="worry_detail_no_prscr_wrapper">
+								<div className="worry_detail_no_prscr_title_wrapper">
+									{isNoRecommendBook ? (
+										<>
+											<p>고민에 맞는 책을 찾지 못했어요</p>
+											<img
+												src="/images/book-detail/write_prscr_img_2.png"
+												id="no_recommend_book_img"
+											/>
+										</>
+									) : (
+										<p>AI 처방이 작성되었어요</p>
+									)}
+								</div>
 							</div>
-						</div>
+							<div className="worry_detail_prscr_wrapper">
+								<div className="wd_prscr_list_title">처방전 확인하기</div>
+								<div className="wd_prscr_list_wrapper">
+									{prescriptionData.length > 0 ? (
+										// 다른 사람 처방이 있는 경우
+										prescriptionData.map((prescription) => (
+											<WidePrscrCard
+												key={prescription.id}
+												props={prescription}
+											/>
+										))
+									) : (
+										// 다른 사람 처방이 없는 경우
+										<>
+											<p>작성된 처방전이 없어요</p>
+											<img
+												src="/images/worry_detail/no_prscr_img.png"
+												id="no_prscr_img"
+											/>
+										</>
+									)}
+								</div>
+							</div>
+						</>
+					) : prescriptionData.length > 0 ? (
+						<>
+							<div className="worry_detail_no_prscr_wrapper">
+								<div className="worry_detail_no_prscr_title_wrapper">
+									<p>{boardData.nickname}님을 위한 AI 처방이 준비 중입니다!</p>
+									<p>조금만 기다려주세요!</p>
+								</div>
+								<img
+									src="/images/book-detail/write_prscr_img_2.png"
+									id="no_prscr_img"
+								/>
+							</div>
+							<div className="worry_detail_prscr_wrapper">
+								<div className="wd_prscr_list_title">처방전 확인하기</div>
+								<div className="wd_prscr_list_wrapper">
+									{prescriptionData.map((prescription) => (
+										<WidePrscrCard key={prescription.id} props={prescription} />
+									))}
+								</div>
+							</div>
+						</>
 					) : (
 						<div className="worry_detail_no_prscr_wrapper">
 							<div className="worry_detail_no_prscr_title_wrapper">
-								<p>작성된 AI 처방이 없어요</p>
-								<p>기다려주세요!</p>
+								<p>작성된 처방전이 없어요</p>
+								<p>{boardData.nickname} 님을 위한 처방전을 작성해보세요!</p>
 							</div>
 
 							<img
@@ -209,6 +299,7 @@ const WorryDetail = () => {
 							/>
 						</div>
 					)}
+
 					<button onClick={() => movePrescriptionWrite()} className="prscr_btn">
 						처방하러 가기
 					</button>
