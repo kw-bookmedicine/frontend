20b2f2a512d75241637a099e49c9d255fe14b2a4
Daewony <alexnori.com@gmail.com>
2024-06-08 10:36:12 +0900

    :bug: Fix: 처방전 수정에서 책 검색 클릭 시, 이미지 수정 안되는 버그 수정

--- src/pages/Prescription/PrescriptionWrite.jsx
+++ src/pages/Prescription/PrescriptionWrite.jsx
@@ -191,14 +191,9 @@ const PrescriptionWrite = () => {
     }
   };
 
-  // const imageUrl =
-  //   location.state?.imageUrl ??
-  //   (modalIsClick ? choiceItem.imageUrl ?? loading_img : loading_img);
   const imageUrl = modalIsClick
     ? choiceItem.imageUrl ?? loading_img
     : location.state?.imageUrl ?? loading_img;
-
-  console.log(imageUrl);
 
   return (
     <>
