7f8b19ea29d99fbe515f9576d7f02164498edc73
Youngbae1126 <rcplaza2663@gmail.com>
2024-11-12 23:01:20 +0900

    :recycle: Refactor: 프로필 기본 이미지 추가

--- src/components/PrescriptionReviewCard.jsx
+++ src/components/PrescriptionReviewCard.jsx
@@ -1,30 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { toYYYYMMDD } from "./../util/toYYYYMMDD";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { toYYYYMMDD } from './../util/toYYYYMMDD';
 
 const PrescriptionReviewCard = ({ post }) => {
-  return (
-    <li className="LoginHome-today-reviews-items-card">
-      {/* <p className="LoginHome-today-reviews-items-card-date">{createdDate}</p> */}
-      <Link to={`/worry-detail?board=${post.boardId}`}>
-        <div className="LoginHome-today-reviews-items-card-body">
-          <div className="LoginHome-today-reviews-items-card-book">
-            <h2>{post.title}</h2>
-            <h3>{post.content}</h3>
-          </div>
-        </div>
-      </Link>
-      <div className="LoginHome-today-reviews-items-card-footer">
-        <div className="LoginHome-user-img-container">
-          {post.userImg ? <img src={post.userImg} alt="유저 이미지" /> : null}
-        </div>
-        <div>
-          <p>{post.nickname}</p>
-          <p className="LoginHome-card-date">{toYYYYMMDD(post.createdDate)}</p>
-        </div>
-      </div>
-    </li>
-  );
+	return (
+		<li className='LoginHome-today-reviews-items-card'>
+			{/* <p className="LoginHome-today-reviews-items-card-date">{createdDate}</p> */}
+			<Link to={`/worry-detail?board=${post.boardId}`}>
+				<div className='LoginHome-today-reviews-items-card-body'>
+					<div className='LoginHome-today-reviews-items-card-book'>
+						<h2>{post.title}</h2>
+						<h3>{post.content}</h3>
+					</div>
+				</div>
+			</Link>
+			<div className='LoginHome-today-reviews-items-card-footer'>
+				<div className='LoginHome-user-img-container'>
+					{post.userImg ? (
+						<img src={post.userImg} alt='유저 이미지' />
+					) : (
+						<img src='/icon/profile/basic_profile_img.svg' alt='프로필' />
+					)}
+				</div>
+				<div>
+					<p>{post.nickname}</p>
+					<p className='LoginHome-card-date'>{toYYYYMMDD(post.createdDate)}</p>
+				</div>
+			</div>
+		</li>
+	);
 };
 
 export default PrescriptionReviewCard;
